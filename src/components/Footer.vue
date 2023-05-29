d7f847c6de0e795b9875950537d9489b99956a9f
vincenzodivita <divitavincenzo@icloud.com>
2023-05-29 19:22:41 +0200

    Add draft of footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,22 @@
 </script>
 
 <template>
-    <h1>TEST MESSAGE FROM FOOTER</h1>
+
+    <div class="container">
+        <img src="../img/logo_seo_1x.png" alt="">
+        <div>NAVBAR</div>
+        <div>COPYRIGHT</div>
+        <div>SOCIAL</div>
+    </div>
+
 </template>
 
 <style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 2rem;
+}
 
 </style>
