d70b6e0a0542bf36bed8b9b2c32ce106440838d9
vincenzodivita <divitavincenzo@icloud.com>
2023-05-29 14:46:28 +0200

    Import Jumbo.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,11 @@
 <script>
 import Navbar from "./Navbar.vue"
+import Jumbo from "./Jumbo.vue"
 
   export default {
       components: {
         Navbar,
-        
+        Jumbo,
       }
     }
 </script>
@@ -12,6 +13,7 @@ import Navbar from "./Navbar.vue"
 <template>
     <h1>TEST MESSAGE FROM HEADER</h1>
     <Navbar/>
+    <Jumbo/>
 </template>
 
 <style scoped>
