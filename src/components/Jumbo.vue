c1fdc369799d6a7f32252bd650c35143f2b0bf65
vincenzodivita <divitavincenzo@icloud.com>
2023-05-29 16:51:44 +0200

    Add draft of jumbo content

--- src/components/Jumbo.vue
+++ src/components/Jumbo.vue
@@ -3,9 +3,43 @@
 </script>
 
 <template>
-    <h2>TEST MESSAGE FROM JUMBO</h2>
+    <div class="contain">
+        <h1>SEM Campaings Made Simple With Avada</h1>
+        <p>
+            <span>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </span> 
+            <span>
+                Corporis cupiditate. Voluptas, placeat?
+            </span>
+                
+        </p>
+        <div class="buttons">
+            <button>Buy Avada Now</button>
+            <button>Contact Us</button>
+        </div>
+    </div>
+    
+
 </template>
 
 <style scoped>
+    .contain {
+        width: 50%;
+        margin-top: 150px;
+    }
+
+    h1 {
+        font-size: 2.5rem;
+    }
+
+    p {
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+    }
+
+    span {
+        display: block;
+    }
 
 </style>
