17b0873af23d74257517a4d2178f53c69bdb9a16
vincenzodivita <divitavincenzo@icloud.com>
2023-05-29 14:41:20 +0200

    Import Navbar.vue

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,17 @@
 <script>
+import Navbar from "./Navbar.vue"
 
+  export default {
+      components: {
+        Navbar,
+        
+      }
+    }
 </script>
 
 <template>
     <h1>TEST MESSAGE FROM HEADER</h1>
+    <Navbar/>
 </template>
 
 <style scoped>
