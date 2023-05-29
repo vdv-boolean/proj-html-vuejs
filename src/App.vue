7dc2a10241153f80e5a1379f355cc8cfee97f843
vincenzodivita <divitavincenzo@icloud.com>
2023-05-29 14:31:50 +0200

    Import Header.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
-<script setup>
-
+<script>
+import Header from "./components/Header.vue"
+export default {
+    components: {
+      Header,
+    }
+  }
 </script>
 
 <template>
-  
+  <Header/>
 </template>
 
 <style scoped>
