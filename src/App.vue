88eb7051db3c28662ccd845f7c80ba10a3d85764
vincenzodivita <divitavincenzo@icloud.com>
2023-05-29 14:40:56 +0200

    Add better indentation

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <script>
-import Header from "./components/Header.vue"
-import Main from "./components/Main.vue"
-import Footer from "./components/Footer.vue"
-export default {
-    components: {
-      Header,
-      Main,
-      Footer,
+
+  import Header from "./components/Header.vue"
+  import Main from "./components/Main.vue"
+  import Footer from "./components/Footer.vue"
+
+  export default {
+      components: {
+        Header,
+        Main,
+        Footer,
+      }
     }
-  }
+    
 </script>
 
 <template>
+
   <Header/>
   <Main/>
   <Footer/>
+
 </template>
 
 <style scoped>
