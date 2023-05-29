75d0964d8cbff9c8cdea829252dead2b247bf73c
vincenzodivita <divitavincenzo@icloud.com>
2023-05-29 14:33:49 +0200

    Import Main.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script>
 import Header from "./components/Header.vue"
+import Main from "./components/Main.vue"
 export default {
     components: {
       Header,
+      Main,
     }
   }
 </script>
 
 <template>
   <Header/>
+  <Main/>
 </template>
 
 <style scoped>
