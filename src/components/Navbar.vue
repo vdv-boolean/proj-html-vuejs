458575b702f08d6688e4d4e248af2f45a502238e
vincenzodivita <divitavincenzo@icloud.com>
2023-05-29 15:50:25 +0200

    Add draft of nav

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,61 @@
 <script>
 
+    export default {
+        data() {
+            return {
+                linkArr: [
+                    "Home",
+                    "Who We Are",
+                    "What We Do",
+                    "Where We Work",
+                    "Careers",
+                    "News",
+                    "GET IN TOUCH NOW"
+                ],
+            };
+        },
+    }
+
 </script>
 
 <template>
-    <h2>TEST MESSAGE FROM NAVBAR</h2>
+
+    <nav>
+        <div>
+            Logo
+            <!-- TO DO: ADD LOGO-->
+        </div>
+        <ul >
+            <li v-for="link in linkArr">
+                <a href="#">
+                    {{ link }}
+                </a>
+            </li>
+        </ul>
+    </nav>
+    
 </template>
 
-<style scoped>
+<style>
+
+    nav {
+        display: flex;
+        justify-content: space-between;
+        color: white;
+    }
+
+    ul {
+        display: flex;
+        gap: 1rem;
+    }
+
+    li {
+        list-style: none;
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+    }
 
 </style>
