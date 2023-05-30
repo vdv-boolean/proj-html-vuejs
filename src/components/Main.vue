b57c0d26189dd89b3572b0733a90eae0f222dddc
vincenzodivita <divitavincenzo@icloud.com>
2023-05-30 15:27:24 +0200

    Fix components import

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -14,6 +14,8 @@
         SectionThree,
         SectionFour,
         SectionFive,
+        SectionSix,
+        SectionSeven,
       }
     }
 </script>
@@ -24,6 +26,8 @@
     <SectionThree/>
     <SectionFour/>
     <SectionFive/>
+    <SectionSix/>
+    <SectionSeven/>
 </template>
 
 <style scoped>
