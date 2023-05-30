f41a00340a10651373056e3ca31b00438af73c89
vincenzodivita <divitavincenzo@icloud.com>
2023-05-30 14:28:02 +0200

    Add draft of Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,9 @@
 
     <div class="container">
         <img src="../img/logo_seo_1x.png" alt="">
-        <div>NAVBAR</div>
+        <div class="nav-bar">
+            <!-- Inserire navbar-->
+        </div>
         <div>COPYRIGHT</div>
         <div>SOCIAL</div>
     </div>
