5455f19bc9f76d332004534b91b1aa2d0c6fd82e
vincenzodivita <divitavincenzo@icloud.com>
2023-05-30 15:42:49 +0200

    Better indentation

--- src/components/SectionOne.vue
+++ src/components/SectionOne.vue
@@ -1,27 +1,27 @@
 <script>
 
-export default {
-    data() {
-        return {
-            cardOne: [{
-                icon: "iconsrc",
-                title: "Estabilish Goals",
-                description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est hic alias voluptates, at debitis dicta deleniti? Tempora nihil debitis, quas animi beatae obcaecati ex laborum vel neque, iusto modi corporis!",
-            },
-            {
-                icon: "iconsrc",
-                title: "Work With a Team",
-                description: "Assumenda asperiores dolorem autem quas obcaecati. Nobis quas culpa saepe eaque quaerat! Cum quibusdam rerum delectus temporibus neque aliquid illo, inventore optio. Fugit incidunt, unde vero veniam aliquid neque rerum.",
-            },
-            {
-                icon: "iconsrc",
-                title: "Gets Results",
-                description: "Dolor voluptatem pariatur aliquam quam atque iusto saepe rerum et, tempora, optio iure! Sit molestias pariatur, nesciunt quae earum eius officiis aliquam nam laudantium, ut sequi. Quibusdam minus sed doloribus.",
-            },
-        ]
-        };
-    },
-}
+    export default {
+        data() {
+            return {
+                cardOne: [{
+                    icon: "iconsrc",
+                    title: "Estabilish Goals",
+                    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est hic alias voluptates, at debitis dicta deleniti? Tempora nihil debitis, quas animi beatae obcaecati ex laborum vel neque, iusto modi corporis!",
+                },
+                {
+                    icon: "iconsrc",
+                    title: "Work With a Team",
+                    description: "Assumenda asperiores dolorem autem quas obcaecati. Nobis quas culpa saepe eaque quaerat! Cum quibusdam rerum delectus temporibus neque aliquid illo, inventore optio. Fugit incidunt, unde vero veniam aliquid neque rerum.",
+                },
+                {
+                    icon: "iconsrc",
+                    title: "Gets Results",
+                    description: "Dolor voluptatem pariatur aliquam quam atque iusto saepe rerum et, tempora, optio iure! Sit molestias pariatur, nesciunt quae earum eius officiis aliquam nam laudantium, ut sequi. Quibusdam minus sed doloribus.",
+                },
+            ]
+            };
+        },
+    }
 
 </script>
 
@@ -43,7 +43,7 @@ export default {
                 <div>{{ card.description }}</div>
             </li>
         </ul>
-        
+
         <img src="../img/agency-seo-desk-front-1200x570.jpg" alt="">
     </section>
     
