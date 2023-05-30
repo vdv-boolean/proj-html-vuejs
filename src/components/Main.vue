a14e2447391d4ca163bcd428cdd3fa2c30c18f6f
vincenzodivita <divitavincenzo@icloud.com>
2023-05-30 14:35:26 +0200

    Import sections to Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,9 +1,27 @@
 <script>
+  import SectionOne from "./SectionOne.vue"
+  import SectionTwo from "./SectionTwo.vue"
+  import SectionThree from "./SectionThree.vue"
+  import SectionFour from "./SectionFour.vue"
+  import SectionFive from "./SectionFive.vue"
 
+  export default {
+      components: {
+        SectionOne,
+        SectionTwo,
+        SectionThree,
+        SectionFour,
+        SectionFive,
+      }
+    }
 </script>
 
 <template>
-    <h1>TEST MESSAGE FROM MAIN</h1>
+    <SectionOne/>
+    <SectionTwo/>
+    <SectionThree/>
+    <SectionFour/>
+    <SectionFive/>
 </template>
 
 <style scoped>
