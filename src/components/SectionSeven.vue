4f1ef5632c465aa76da6b45a7d90a7a07854b53f
vincenzodivita <divitavincenzo@icloud.com>
2023-05-30 15:27:39 +0200

    add draft of content

---
+++ src/components/SectionSeven.vue
@@ -0,0 +1,25 @@
+<script>
+
+</script>
+
+<template>
+
+    <section>
+        <h1>
+            Start Your Free Consultation
+        </h1>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsum consectetur autem ipsam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsum consectetur autem ipsam!
+        </p>
+
+        <button>   
+            Contact Us Now
+        </button>
+    </section>
+    
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/SectionSix.vue
@@ -0,0 +1,18 @@
+<script>
+
+</script>
+
+<template>
+
+    <section>
+        <h1>
+            What Our Client Says
+        </h1>
+        
+    </section>
+    
+</template>
+
+<style scoped>
+
+</style>
