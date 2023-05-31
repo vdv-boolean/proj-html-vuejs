2363b402f9447402427f841760818ced61a77095
vincenzodivita <divitavincenzo@icloud.com>
2023-05-31 14:48:36 +0200

    Create component and past props for loop cycle

---
+++ src/components/Menu.vue
@@ -0,0 +1,58 @@
+<script>
+
+    export default {
+        
+        props: {
+            title: String,
+            link: String,
+            isActive: String,
+            label: String,
+        },
+    };
+    
+</script>
+
+<template>
+
+        <li >
+            <button>
+               <a
+                    :href="link"
+                    :class="isActive"
+                >
+                    {{ title }}
+                    <span>
+                        {{ label }}
+                    </span>
+               </a> 
+            </button>
+        </li>
+   
+</template>
+
+<style scoped>
+
+    button {
+        background-color: none;
+        background: none;
+        border: none;
+    }
+
+    a {
+        text-decoration: none;
+        color: white;
+    }
+
+    .active {
+        color: var(--orange);
+    }
+
+    span {
+        color: var(--orange);
+        border: 1px solid var(--orange);
+        font-size: 0.5rem;
+        padding: 0.2rem;
+        font-weight: bolder;
+    }
+
+</style>
