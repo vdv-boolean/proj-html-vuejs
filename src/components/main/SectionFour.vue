cb84869c0957cfea45fcc96fe7d11ccfbc9487f6
vincenzodivita <divitavincenzo@icloud.com>
2023-05-31 11:04:08 +0200

    Add draft of content

--- src/components/main/SectionFour.vue
+++ src/components/main/SectionFour.vue
@@ -1,5 +1,41 @@
 <script>
 
+    export default {
+        data() {
+            return {
+                cardFour: [{
+                    name: "Standard",
+                    price: "$19.99 monthly",
+                    projects: "5 Projects",
+                    storage: "5 GB Storage",
+                    users: "Unlimited Users",
+                },
+                {
+                    name: "Premium",
+                    price: "$29.99 monthly",
+                    projects: "10 Projects",
+                    storage: "15 GB Storage",
+                    users: "Unlimited Users",
+                },
+                {
+                    name: "Professional",
+                    price: "$39.99 monthly",
+                    projects: "15 Projects",
+                    storage: "30 GB Storage",
+                    users: "Unlimited Users",
+                },
+                {
+                    name: "Extreme",
+                    price: "$59.99 monthly",
+                    projects: "Unlimited Projects",
+                    storage: "Unlimited Storage",
+                    users: "Unlimited Users",
+                },
+            ]
+            };
+        },
+    }
+
 </script>
 
 <template>
@@ -12,6 +48,31 @@
         <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsum consectetur autem ipsam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsum consectetur autem ipsam!
         </p>
+
+        <div
+            v-for="card in cardFour"
+        >
+            <ul>
+                <li>
+                    {{ card.name }}
+                </li>
+                <li>
+                    {{ card.price }}
+                </li>
+                <li>
+                    {{ card.projects }}
+                </li>
+                <li>
+                    {{ card.storage }}
+                </li>
+                <li>
+                    {{ card.users }}
+                </li>
+                <button>
+                    <a href="">Start TODAY</a>
+                </button>
+            </ul>
+        </div>
     </section>
     
 </template>
