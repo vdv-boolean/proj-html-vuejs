215d3ed33cbe1c67b99ac5e9ce1a08896403a3fd
vincenzodivita <divitavincenzo@icloud.com>
2023-05-31 17:17:36 +0200

    Add element and css

--- src/components/main/cards/CardThree.vue
+++ src/components/main/cards/CardThree.vue
@@ -14,14 +14,39 @@ export default {
         <img 
         :src="img"
         :alt="alt"
-    > 
+        > 
+        <button>
+            <a :href="link">link</a>
+        </button>
     </div>
+
      
 </template>
 
 <style scoped>
+
+div {
+    position: relative;
+}
+
 img {
     width: 100%;
     height: 100%;
 }
+
+button {
+    width: 100%;
+    height:100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    opacity: 0%;
+    border-radius: 0%;
+}
+
+button:hover {
+    opacity: 100%;
+    background: linear-gradient(var(--violet), transparent);
+}
+
 </style>
