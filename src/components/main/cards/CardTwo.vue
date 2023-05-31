96f179926c2bd968013b35a5cae3a87e10aae832
vincenzodivita <divitavincenzo@icloud.com>
2023-05-31 11:35:19 +0200

    Add component for 2nd section card

---
+++ src/components/main/cards/CardTwo.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+	
+	props: {
+		icon: String,
+		title: String,
+		description: String,
+        link: String,
+	},
+};
+</script>
+
+<template>
+    <ul>
+        <li>
+            {{ icon }}
+        </li>
+        <li>
+            {{ title }}
+        </li>
+        <li>
+            {{ description }}
+        </li>
+        <li>
+            <button>
+                <a 
+                    :href="link"
+                >   
+                    Read More...
+                </a>
+            </button>
+        </li>
+    </ul>
+    
+</template>
+
+<style scoped>
+
+</style>
