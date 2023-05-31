c712f07f1d7c5dea3794b94719611d47e159f902
vincenzodivita <divitavincenzo@icloud.com>
2023-05-31 16:02:01 +0200

    Add background and style

--- src/components/main/SectionSeven.vue
+++ src/components/main/SectionSeven.vue
@@ -5,21 +5,45 @@
 <template>
 
     <section>
-        <h1>
-            Start Your Free Consultation
-        </h1>
 
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsum consectetur autem ipsam! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsum consectetur autem ipsam!
-        </p>
+        <div class="container">
+            <h1>
+                Start Your Free Consultation
+            </h1>
 
-        <button>   
-            Contact Us Now
-        </button>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ipsum consectetur autem ipsam!
+            </p>
+
+            <button class="orange-btn">   
+                <a href="">
+                    Contact Us Now
+                </a>
+            </button>
+        </div>
     </section>
     
 </template>
 
 <style scoped>
+
+    section {
+        background-image: url(../../../public/img/CTA-seo-1.jpg);
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position: center;
+        height: 300px;
+    }
+
+    .container {
+        padding-top: 90px;
+        padding-bottom: 90px;
+        color: white;
+    }
+
+    p {
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+    }
 
 </style>
